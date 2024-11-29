faffb5f270aedac1a60634e859ae164febd27486
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2024-11-29 16:10:12 +0530

    Header is Finished.

--- src/components/Header/HApp.tsx
+++ src/components/Header/HApp.tsx
@@ -1,26 +1,36 @@
-import DateOption from "./DateOption";
-import SearchBar from "./SearchBar";
+import DateOption from "./HeadComponents/DateOption";
+import SearchBar from "./HeadComponents/SearchBar";
 import styled from "styled-components";
-import SearchBtn from "./SearchBtn";
+import SearchBtn from "./HeadComponents/SearchBtn";
 
-const Flex = styled.div`
-  padding: 10px;
-  margin: 10px;
-  width: 700px;
-  background-color: aliceblue;
-
+//Common Style for both divs
+const FlexType = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
 `;
 
+const HeadContainer = styled(FlexType)`
+  margin-top: 60px;
+  flex-direction: column;
+`;
+
+const Flex = styled(FlexType)`
+  padding: 10px;
+  margin: 10px;
+  width: 40%;
+`;
+
 const HApp = () => {
   return (
-    <Flex id="Header">
-      <SearchBar />
-      <SearchBtn />
-      <DateOption />
-    </Flex>
+    <HeadContainer>
+      <h1>Parking Space Availability</h1>
+      <Flex id="Header">
+        <SearchBar />
+        <SearchBtn />
+        <DateOption />
+      </Flex>
+    </HeadContainer>
   );
 };
 
--- src/components/Header/HeadComponents/SearchBar.tsx
+++ src/components/Header/HeadComponents/SearchBar.tsx
@@ -9,10 +9,7 @@ const SearchBar = () => {
   return (
     <>
       <p>Location</p>
-      <Place
-        className="form-select form-select-lg"
-        aria-label="Large select example"
-      >
+      <Place className="form-select form-select-lg">
         <option selected>Open this select menu</option>
         <option value="1">Singanallur</option>
         <option value="2">Sulur</option>
